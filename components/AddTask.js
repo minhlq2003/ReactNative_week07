c3b4cfca5f5459c4d00d8240c1eb6d40cc1fb7e2
Ly Quoc Minh <minh@gmail.com>
2024-10-13 14:45:24 +0700

    Edit, Delete Task

--- components/AddTask.js
+++ components/AddTask.js
@@ -1,37 +1,48 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-paper";
 
-const handleAddTask = async (title) => {
-    try {
-        const response = await fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                title: title,
-                isFinished: false
-            })
-        });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        console.log('Task added:', data);
-    } catch (error) {
-        console.error('Error adding task:', error);
-    }
-};
-function AddTask() {
+function AddTask( ) {
     const [title, setTitle] = useState("");
+    const navigation = useNavigation();
+    const route = useRoute();
+    const fetchTask = route.params.fetchTask;
+    
+    const handleAddTask = async (title) => {
+        try {
+            const response = await fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: title,
+                    isFinished: false
+                })
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+            console.log('Task added:', data);
+            fetchTask();
+        } catch (error) {
+            console.error('Error adding task:', error);
+        }
+    };
+
     return (  
         <View>
             <Text style={{alignSelf:"center", fontSize:30, paddingBottom: 50}}>Add Task</Text>
             <TextInput placeholder="input your task" onChangeText={(value)=>setTitle(value)} value={title}/>
-            <TouchableOpacity onPress={()=> handleAddTask(title)} style={{backgroundColor:"blue"}}>
+            <TouchableOpacity onPress={()=> {
+                handleAddTask(title)
+                navigation.navigate('TaskList');
+            }} style={{backgroundColor:"blue"}}>
                 <Text style={{alignSelf: "center", fontSize: 20, color:"white"}}>
                     Finish 
                 </Text>
--- components/TaskList.js
+++ components/TaskList.js
@@ -1,75 +1,164 @@
 import { useNavigation } from "@react-navigation/native";
 import { useState, useEffect } from "react";
-import { FlatList, SafeAreaView, View , Text, TouchableOpacity} from "react-native";
+import { FlatList, SafeAreaView, View , Text, TouchableOpacity, TextInput, ActivityIndicator} from "react-native";
 import { Checkbox } from "react-native-paper";
 
-
-const handleCheckTask = async (id, isFinished) => {
-    try {
-        const response = await fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                isFinished: !isFinished
-            })
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        console.log('Task updated:', data);
-    } catch (error) {
-        console.error('Error updating task:', error);
-    }
-}
-
-const Item = ({id, title, isFinished }) => {
-    return (
-        <View style={{flexDirection: "row", justifyContent:"space-between"}}>
-            <Text>{title}</Text>
-            <Checkbox
-                status={isFinished ? 'checked' : 'unchecked'}
-                onPress={() => {
-                   handleCheckTask(id, isFinished);
-                }}
-            />
-        </View>
-    );
-};
 
 function TaskList() {
     const [task, setTask] = useState([]);
     const navigation = useNavigation();
+    const [loading, setLoading] = useState(false);
+    const [editStates, setEditStates] = useState({});
+
+    const handleCheckTask = async (id, isFinished) => {
+        setLoading(true);
+        try {
+            const response = await fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    isFinished: !isFinished
+                })
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+            console.log('Task updated:', data);
+            fetchTask();
+        } catch (error) {
+            console.error('Error updating task:', error);
+        }
+    }
+    const handleDeleteTask = async (id) => {
+        setLoading(true);
+        try {
+            const response = await fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            console.log('Task deleted:', id);
+            fetchTask();
+        } catch (error) {
+            console.error('Error deleting task:', error);
+        }
+    }
+    
+    const handleEditTask = async (id, title) => {
+        try {
+            const response = await fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: title
+                })
+            });
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const data = await response.json();
+            console.log('Task updated:', data);
+            fetchTask();
+        } catch (error) {
+            console.error('Error updating task:', error);
+        }
+        toggleEditState(id);
+    }
+    
+    
+
+    const toggleEditState = (id) => {
+        setEditStates((prevState) => ({
+            ...prevState,
+            [id]: !prevState[id],
+        }));
+
+    };
+    
+    const Item = ({id, title, isFinished }) => {
+        const isEdit = editStates[id] || false;
+        const [newTitle, setNewTitle] = useState(title);
+        return (
+            <View style={{flexDirection: "row", justifyContent:"space-between", padding:5, margin:5, borderRadius:20, backgroundColor: "#ffe"}}>
+                <View style={{flexDirection:"row"}}>
+                    <Checkbox
+                        status={isFinished ? 'checked' : 'unchecked'}
+                        onPress={() => {
+                           handleCheckTask(id, isFinished);
+                        }}
+                    />
+                    {isEdit ? 
+                    (<TextInput
+                        onChangeText={setNewTitle}
+                        value={newTitle}
+                        
+                    />)
+                    :(<Text style={{paddingTop:10}}>{title}</Text>)}
+                </View>
+                <View style={{flexDirection: "row", marginTop: 10, paddingRight: 10}}>
+                    <TouchableOpacity style={{borderRadius:5, marginRight:10}} onPress={()=>{handleDeleteTask(id)}}>
+                        <Text style={{color:"red"}}>Delete</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{isEdit? handleEditTask(id, newTitle): toggleEditState(id)}}>
+                        {isEdit? 
+                        (<Text style={{color:"blue"}}>Save</Text>): (<Text style={{color:"blue"}}>Edit</Text>)}
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    };
     
     useEffect(() => {
-        setTask([]);
-        fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((response) => response.json())
-            .then((data) => {
-                setTask(data);
-            });
+        fetchTask();
+        console.log('TaskList rendered');
+    }, []);
 
-    }, [handleCheckTask]);
+    const fetchTask = async () => {
+        fetch(`https://670b380aac6860a6c2cb6db9.mockapi.io/todoapi/job`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        }).then((response) => response.json())
+        .then((data) => {
+            setTask(data);
+        });
+        setTimeout(() => {
+            setLoading(false);
+        }, 1500);
+    };
+
 
     return ( 
-        <SafeAreaView>
+        <SafeAreaView style={{margin:10}}>
             <View style={{alignItems:"center", marginBottom: 50}}><Text style={{ fontSize:30}}>TaskList</Text></View>
+            {loading ? (<View style={{height:400}}><ActivityIndicator size="large" color="#0000ff" /></View>)
+            :(<View style={{height:400}}>
             <FlatList
                 data={task}
                 renderItem={({ item }) => <Item id={item.id} title={item.title} isFinished={item.isFinished} />}
                 keyExtractor={item => item.id}
             />
+            </View>)
+            }
             <View>
-                <TouchableOpacity style={{backgroundColor:"blue"}} onPress={()=>{
-                    navigation.navigate("AddTask")
+                <TouchableOpacity style={{backgroundColor:"blue", width:200, alignSelf: "center", borderRadius:10, height:50, justifyContent:"center", marginTop: 20}} onPress={()=>{
+                    navigation.navigate("AddTask", {fetchTask: fetchTask});
                 }} >
                     <Text style={{fontSize: 20, color: "white", textAlign: "center"}}>Add Task</Text>
                 </TouchableOpacity>
